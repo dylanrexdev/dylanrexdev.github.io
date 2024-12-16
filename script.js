9bcd02d9711c916a42d0d490a04a1b000f5e881b
dylanrexdev <dylanrexdev@gmail.com>
2024-12-16 00:45:15 -0500

    MENU WORKS

---
+++ script.js
@@ -0,0 +1,8 @@
+function menu_toggle() {
+    var x = document.getElementById("navbar");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+}
